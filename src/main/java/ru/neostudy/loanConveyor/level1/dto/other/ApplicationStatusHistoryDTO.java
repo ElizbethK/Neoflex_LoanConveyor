a94dfec18fea223b32c6895e8d5e3bc024167fac
ElizbethK <liz.curo4kina2011@yandex.ru>
2023-01-05 01:16:45 +0300

    added DTO

--- src/main/java/ru/neostudy/loanConveyor/level1/dto/other/ApplicationStatusHistoryDTO.java
+++ src/main/java/ru/neostudy/loanConveyor/level1/dto/other/ApplicationStatusHistoryDTO.java
@@ -13,4 +13,10 @@ public class ApplicationStatusHistoryDTO {
     private Enum status;
     private LocalDateTime time;
     private Enum changeType;
+
+    public ApplicationStatusHistoryDTO(Enum status, LocalDateTime time, Enum changeType) {
+        this.status = status;
+        this.time = time;
+        this.changeType = changeType;
+    }
 }
--- src/main/java/ru/neostudy/loanConveyor/level1/dto/other/EmailMessage.java
+++ src/main/java/ru/neostudy/loanConveyor/level1/dto/other/EmailMessage.java
@@ -11,4 +11,10 @@ public class EmailMessage {
     private String address;
     private Enum theme;
     private Long applicationId;
+
+    public EmailMessage(String address, Enum theme, Long applicationId) {
+        this.address = address;
+        this.theme = theme;
+        this.applicationId = applicationId;
+    }
 }
